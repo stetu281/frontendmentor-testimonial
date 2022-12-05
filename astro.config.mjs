f232f39eede86079108938b7a5ae31b228a7cc5c
Stefan Turner <turner.stefan281@gmail.com>
2022-12-05 09:41:48 +0100

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind({
     config: { applyBaseStyles: false },
-  })]
+  })],
+  outDir: './docs',
 });
